564bf256f39cf696650579858bf029b355fefa5a
Sridevi Ramanujam <srideviramanujam@Sridevis-Air.home>
2017-01-23 13:46:31 -0500

    changing from maintenance to normal mode

--- server.js
+++ server.js
@@ -28,9 +28,9 @@ app.use((req,res,next) => {
 });
 
 // app.use for maintenance hbs
-app.use((req,res,next) => {
-  res.render('maintenance.hbs');
-});
+// app.use((req,res,next) => {
+//   res.render('maintenance.hbs');
+// });
 
 app.use(express.static(__dirname + '/public'));
 
